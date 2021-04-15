30e0f1f075418defea24f903cc9a485f3e322f67
Wellinton Perazzoli <wellctba@gmail.com>
2021-04-15 11:12:49 -0300

    funcionou

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Tabletop from 'tabletop';
 
-function App() {
-  return (
-    <main>
-      <h1>Olá mundo! 2 </h1>
-    </main>
-  );
+class App extends Component {
+  state = {
+    data : {},
+  };
+  
+
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1c_b6jA-wJsEGmELpZN9mVo4wrBxVo5q18-u5Xo49n1w',
+      callback: googleData => {
+        this.setState({ 
+          data :  googleData,     
+        });
+      },
+      simpleSheet: true
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <table>
+            <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((i, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{i.nome}</td>
+                    <td>{i.valor}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
